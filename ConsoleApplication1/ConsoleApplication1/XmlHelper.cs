f18bf6bfd0d9d70ee34d4d0ef3426762dd21fcb5
anxinjc <anxinjc@qq.com>
2017-02-10 17:17:46 +0800

    补充例子

--- ConsoleApplication1/ConsoleApplication1/XmlHelper.cs
+++ ConsoleApplication1/ConsoleApplication1/XmlHelper.cs
@@ -30,12 +30,26 @@ namespace ConsoleApplication1
         {
             base.RemoveAllChilds(base.rootName);
 
+            //添加节点
             base.InsertNode("personList", "person0", "Han Mei Mei");
-            base.InsertNode("personList", "person1", "Li Lei", "Age", "18");
+            base.InsertNode("personList", "person0", "Lucy");
+
+            //第一个【personList/person0】的【age】属性为【14】
+            base.SetAttribute("personList/person0", "age", "14");
+
+            //第二个【personList/person0】的【age】属性为【17】
+            base.SetAttribute("personList/person0[2]", "age", "17");
+
+            //添加带有属性的节点
+            base.InsertNode("personList", "person1", "Jim", "age", "21");
+            base.InsertNode("personList", "person1", "", "age", "18");
+
+            base.InsertNode("personList/person1[@age='18']", "school", "Bei Jing Da Xue");
             
 
+
             base.InsertNode(base.rootName, "aaa", "inner");
-            base.SetAttribute(base.rootName + "/aaa[@name='aaa-name2']", "sex", "female");
+            //base.SetAttribute(base.rootName + "/aaa[@name='aaa-name2']", "sex", "female");
 
             
         }
